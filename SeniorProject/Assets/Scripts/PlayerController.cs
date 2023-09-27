a26af89dbf1072d05d73cbd4f7753641ef548b20
adt80670 <adt80670@ucmo.edu>
2023-09-27 13:16:37 -0500

    SampleTest

---
+++ SeniorProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private float minX = -5.5f;
+    private float maxX = 5.5f;
+    private float minY = -4.5f;
+    private float maxY = 4.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        Vector2 currentPosition = transform.position;
+        currentPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        if (currentPosition.x < minX)
+            currentPosition.x = minX;
+        if (currentPosition.x > maxX)
+            currentPosition.x = maxX;
+
+        currentPosition.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        if (currentPosition.y < minY)
+            currentPosition.y = minY;
+        if (currentPosition.y > maxY)
+            currentPosition.y = maxY;
+
+        transform.position = currentPosition;
+    }
+}
